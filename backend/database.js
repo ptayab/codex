f9d1ee221af3e0fa21b7228b9f0e7e091bc125fa
ptayab <prt898@usask.ca>
2023-12-05 03:56:38 -0600

    Likes and Dislike buttons completed

--- backend/database.js
+++ backend/database.js
@@ -68,6 +68,8 @@ db.connect((err) => {
         user_id int NOT NULL,
         channel_id int NOT NULL,
         images JSON,
+        likes int DEFAULT 0,
+        dislikes int DEFAULT 0,
         FOREIGN KEY (user_id) REFERENCES users(id),
         FOREIGN KEY (channel_id) REFERENCES channels(id)
     )
@@ -85,8 +87,8 @@ db.connect((err) => {
     CREATE TABLE IF NOT EXISTS comments (
         id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
         comment TEXT NOT NULL,
-        likes int,
-        dislikes int,
+        likes int DEFAULT 0,
+        dislikes int DEFAULT 0,
         user_id int NOT NULL,
         post_id int NOT NULL,
         FOREIGN KEY (user_id) REFERENCES users(id),
@@ -105,8 +107,8 @@ db.connect((err) => {
     CREATE TABLE IF NOT EXISTS replies (
         id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
         reply TEXT NOT NULL,
-        likes int,
-        dislikes int,
+        likes int DEFAULT 0,
+        dislikes int DEFAULT 0,
         user_id int NOT NULL,
         comment_id int NOT NULL,
         FOREIGN KEY (user_id) REFERENCES users(id),
@@ -119,7 +121,112 @@ db.connect((err) => {
         console.log('Table "replies" created or already exists');
     }
     });
+
+
+    // create commentlikes table
+    db.query(`
+    CREATE TABLE IF NOT EXISTS commentlikes (
+        id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        user_id int NOT NULL,
+        comment_id int NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        FOREIGN KEY (comment_id) REFERENCES comments(id)
+    )
+`, (err) => {
+    if (err) {
+        console.log('Error creating commentlikes table:', err.message);
+    } else {
+        console.log('Table "commentlikes" created or already exists');
+    }
+    });
+
+    // create postlikes table
+    db.query(`
+    CREATE TABLE IF NOT EXISTS postlikes (
+        id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        user_id int NOT NULL,
+        post_id int NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        FOREIGN KEY (post_id) REFERENCES posts(id)
+    )
+`, (err) => {
+    if (err) {
+        console.log('Error creating postlikes table:', err.message);
+    } else {
+        console.log('Table "postlikes" created or already exists');
+    }
+    });
+
+    // create replylikes table
+    db.query(`
+    CREATE TABLE IF NOT EXISTS replylikes (
+        id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        user_id int NOT NULL,
+        reply_id int NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        FOREIGN KEY (reply_id) REFERENCES replies(id)
+    )
+`, (err) => {
+    if (err) {
+        console.log('Error creating replylikes table:', err.message);
+    } else {
+        console.log('Table "replylikes" created or already exists');
+    }
+    });
+
+    // create replydislikes table
+    db.query(`
+    CREATE TABLE IF NOT EXISTS replydislikes (
+        id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        user_id int NOT NULL,
+        reply_id int NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        FOREIGN KEY (reply_id) REFERENCES replies(id)
+    )
+`, (err) => {  
+    if (err) {
+        console.log('Error creating replydislikes table:', err.message);
+    } else {
+        console.log('Table "replydislikes" created or already exists');
+    }
+    });
+
+    // create postdislikes table
+    db.query(`
+    CREATE TABLE IF NOT EXISTS postdislikes (
+        id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        user_id int NOT NULL,
+        post_id int NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        FOREIGN KEY (post_id) REFERENCES posts(id)
+    )
+`, (err) => {
+    if (err) {
+        console.log('Error creating postdislikes table:', err.message);
+    } else {
+        console.log('Table "postdislikes" created or already exists');
+    }
 });
+
+
+    // create commentdislikes table
+    db.query(`
+    CREATE TABLE IF NOT EXISTS commentdislikes (
+        id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        user_id int NOT NULL,
+        comment_id int NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        FOREIGN KEY (comment_id) REFERENCES comments(id)
+    )
+`, (err) => {
+    if (err) {
+        console.log('Error creating commentdislikes table:', err.message);
+    } else {
+        console.log('Table "commentdislikes" created or already exists');
+    }
+    });
+});
+    
 });
 
 
