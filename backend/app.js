0edac8f46188d225d28dbcf28e9f0bf63d8bf5eb
ptayab <prt898@usask.ca>
2023-12-05 04:00:04 -0600

    Updated app.js (backend)
    Accidentally deleted replies route
    - bug fixed

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const userRoutes = require('./users');
 const channelRoutes = require('./channels');
 const postRoutes = require('./posts');
 const commentRoutes = require('./comments');
+const repliesRoutes = require('./replies');
 
 
 const app = express();
@@ -16,6 +17,7 @@ app.use('/users', userRoutes);
 app.use('/channels', channelRoutes);
 app.use('/posts', postRoutes);
 app.use('/comments', commentRoutes);
+app.use('/replies', repliesRoutes);
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
